2fd710be9e2275871ecaa1dd7f4bea0c4c54c5e4
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2022-05-25 21:07:59 -0300

    express -> adonis

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
-
 const port = 8080
+
+const dotenv = require('dotenv');
+//Configurar o uso das variáveis de ambiente na aplicação.
+dotenv.config()
+
+//Chamando o DB   
+const db = require('./src/database/db')
+// const teste = db.
 
 app.get('/login', ((req, res) => {
     res.status(200).send('Hello World')
