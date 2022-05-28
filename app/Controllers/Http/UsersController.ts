2eb8bc4b7b8be82cf931f626e111be874b4b7fc7
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2022-05-28 11:53:08 -0300

    CRUD - Controller User

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,40 +1,69 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from '../../Models/User'
 
-//Criando um objeto, para inserir diretamente as informações
-const user = new User()
-
 export default class UsersController {
 
-  public async index(ctx: HttpContextContract) {
-      
-      // console.log("Controller User - Método Index" + user),
-      
+  public async index({request, response}) {
+    try {
+      console.log("Controller User - Método Index")
+      // const paramters = request.paramters()
+      // console.log('params', paramters)
+
+      const users = await User.all()
+      // const users = await User
+      //   .query() // 👈now have access to all query builder methods
+      //   .where('id', 4)
+
+      //  console.log("users", users) 
+
+      response.status(200).send({message: "Retornando acesso ao user", data: users})
+    }
+    catch (err) {
+      response.status(200).send({message: "Algo deu errado!", error: err})
+  }
 }
 
-  public async update(ctx: HttpContextContract) {
+
+  public async update({request, response}) {
+    try {
      console.log("Controller User - Método Update")
+     const data = request.body()
+     const params = request.params()
+     console.log("data", data)
+     const user = await User.query().where('id', params.id).update(data)
 
+    response.status(200).send({message: "Uusário atualizado com sucesso", data: data})
+  }
+    catch (err) {
+    response.status(400).send({message: "Não foi possível atualizar o usuário ", error: err})
+  }
   }
 
-  public async create(ctx: HttpContextContract) {
+
+  public async store({request, response}) {
+    try {
       console.log("Controller User - Método Create")
-
-      // user.id = 1
-      user.nome = "a",
-      user.email = "teste@teste.com",
-      user.telefone = 15991851171,
-      user.senha = 'bobito123'
-
-      console.log("user", user)
-    
-      await user.save()
+      const data = request.body()
+      console.log("data", data)
+      await User.create(data)
+      response.status(200).send({message: "Usuário criado com sucesso", data: data})
+    }
+      catch (err) {
+        response.status(400).send({message: "Não foi possível atualizar o usuário ", data: err})
+      }
   }
 
-  public async delete(ctx: HttpContextContract) {
+
+  public async delete({request, response}) {
+    try {
      console.log("Controller User - Método Delete")
-
+     const params = request.params()
+     const user = await User.findOrFail(params.id)
+     await user.delete()
+     return response.status(200).send({message: "Usuário deletado com sucesso", data: user})
+    } catch (err) {
+      response.status(400).send({message: "Não foi possível atualizar o usuário ", data: err})
+    }
+     
   }
-
-
 }
--- start/routes.ts
+++ start/routes.ts
@@ -4,7 +4,7 @@ import UsersController from '../app/Controllers/Http/UsersController'
 Route.group(() => {
   Route.get('user', 'UsersController.index')
   Route.put('user/:id', 'UsersController.update')
-  Route.post('user/', 'UsersController.create')
+  Route.post('user/', 'UsersController.store')
   Route.delete('user/:id', 'UsersController.delete')
 }
 ).prefix('api')
