ca3e74e2a3db84d5e89e2bc5dc62fa416ffcec24
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2022-05-31 20:15:49 -0300

    fix login route | move out of group middleware

--- start/routes.ts
+++ start/routes.ts
@@ -1,12 +1,13 @@
 import Route from '@ioc:Adonis/Core/Route'
 
+
+Route.post('api/login', 'AuthController.login')
+
 Route.group(() => {
   Route.get('user', 'UsersController.index')
   Route.put('user/:id', 'UsersController.update')
   Route.post('user/', 'UsersController.store')
   Route.delete('user/:id', 'UsersController.delete')
-
-  Route.post('login', 'AuthController.login')
 
   Route.get('cripto', 'CriptosController.index')
   Route.put('cripto/:id', 'CriptosController.update')
