4ae1cedcd6b7259679a4eb6acc58bf3ff2217e20
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2022-06-05 11:12:46 -0300

    Fix: Routes no Auth

--- start/routes.ts
+++ start/routes.ts
@@ -2,12 +2,13 @@ import Route from '@ioc:Adonis/Core/Route'
 
 
 Route.post('api/login', 'AuthController.login')
+Route.post('api/user/', 'UsersController.store')
+
 
 Route.group(() => {
   Route.get('user', 'UsersController.index')
   Route.get('user/:id', 'UsersController.show')
   Route.put('user/:id', 'UsersController.update')
-  Route.post('user/', 'UsersController.store')
   Route.delete('user/:id', 'UsersController.delete')
 
   Route.get('cripto', 'CriptosController.index')
