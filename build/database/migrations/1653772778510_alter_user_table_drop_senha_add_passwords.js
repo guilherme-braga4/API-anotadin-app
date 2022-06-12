f70880e48e755c00b953363c58b907c87bacd830
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2022-06-12 11:55:30 -0300

    build and deploy

---
+++ build/database/migrations/1653772778510_alter_user_table_drop_senha_add_passwords.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Schema_1 = __importDefault(require("@ioc:Adonis/Lucid/Schema"));
+class default_1 extends Schema_1.default {
+    constructor() {
+        super(...arguments);
+        this.tableName = 'alter_user_table_drop_senha_add_passwords';
+    }
+    async up() {
+        this.schema.alterTable('users', (table) => {
+            table.dropColumn('senha');
+            table.string('password');
+        });
+    }
+    async down() {
+        this.schema.dropTable(this.tableName);
+    }
+}
+exports.default = default_1;
+//# sourceMappingURL=1653772778510_alter_user_table_drop_senha_add_passwords.js.map
