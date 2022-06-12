becd2466756e21c0705b2630fb90065bca8dd9bd
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2022-06-12 12:18:56 -0300

    fix dependecies

--- build/env.js
+++ build/env.js
@@ -3,13 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// const Env_1 = __importDefault(require("@ioc:Adonis/Core/Env"));
+const Env_1 = __importDefault(require("@ioc:Adonis/Core/Env"));
 exports.default = Env_1.default.rules({
-    HOST: "0.0.0.0",
-    PORT: "3333",
-    APP_KEY: "C-Wq4bPaULdfqYXeiuv0oMao_50yV1tr",
-    APP_NAME: "anotadin-api",
-    DRIVE_DISK: "local",
-    NODE_ENV: "production"
+    HOST: Env_1.default.schema.string({ format: 'host' }),
+    PORT: Env_1.default.schema.number(),
+    APP_KEY: Env_1.default.schema.string(),
+    APP_NAME: Env_1.default.schema.string(),
+    DRIVE_DISK: Env_1.default.schema.enum(['local']),
+    NODE_ENV: Env_1.default.schema.enum(['development', 'production', 'test']),
 });
 //# sourceMappingURL=env.js.map
